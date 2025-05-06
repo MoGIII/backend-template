92c91e74c70e812a936155e8ef268b5c838ad4c2
max gromov <maximgromov@gmail.com>
2025-05-06 12:20:41 +0300

    Added body-parser package

--- src/server.js
+++ src/server.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.get('/', (req,res)=>{
+    res.sendStatus(200);
+});
 
 app.listen(port,()=>{
     console.log(`Listening on http://localhost:${port}`);
